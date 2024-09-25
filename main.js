1d4e4a719b66c973f0d72af5595cef6665ffaa52
victorgileau <vgileau@hotmail.com>
2024-09-24 20:27:48 -0400

    msgProjetEtudant_final-script+adapatabiliter

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-const menuHamburger = document.querySelector(".burgerBtn")
-        const navLinks = document.querySelector(".desktop")
+const menuHamburger = document.querySelector(".burgerBtn");
+        const navLinks = document.querySelector(".desktop");
  
         menuHamburger.addEventListener('click',()=>{
         navLinks.classList.toggle('mobile-menu')
-        })
+        });
+
+
+/*--Code ajouter msg étudant--*/
+const btnMessageProjetEtudiant = document.querySelector('.messageProjetEtudant__closeBtn');
+const messageProjetEtudiant = document.querySelector('.messageProjetEtudant');
+
+if (sessionStorage.getItem('msgFermer') != null) {
+        let msgFermer = sessionStorage.getItem('msgFermer');
+        if (msgFermer == 'true') {
+                console.log('messag projet étudiant déja vu');
+                messageProjetEtudiant.classList.add('displayNone');
+        }
+}
+
+btnMessageProjetEtudiant.addEventListener('click', () => {
+        messageProjetEtudiant.classList.add('displayNone');
+        sessionStorage.setItem('msgFermer', 'true');
+        console.log('test');
+});
+
+/*--Fin Code ajouter msg étudant--*/
