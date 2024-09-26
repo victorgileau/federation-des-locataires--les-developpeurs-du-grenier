5980cf4effa0265a2088cebb513e5a7e48d52289
Jxshvfx <spiteyhd@gmail.com>
2024-09-25 21:50:54 -0400

    ajustement message en haut
    ajout de margin-top pour le menu, modification dans main.js

--- main.js
+++ main.js
@@ -9,16 +9,21 @@ const menuHamburger = document.querySelector(".burgerBtn");
 /*--Code ajouter msg étudant--*/
 const btnMessageProjetEtudiant = document.querySelector('.messageProjetEtudant__closeBtn');
 const messageProjetEtudiant = document.querySelector('.messageProjetEtudant');
+const navBar = document.querySelector(".navbar");
 
 if (localStorage.getItem('msgFermer') != null) {
         let msgFermer = localStorage.getItem('msgFermer');
         if (msgFermer == 'true') {
                 console.log('messag projet étudiant déja vu');
                 messageProjetEtudiant.classList.add('displayNone');
+                navBar.classList.add('noMarginTop');
+                navLinks.classList.add('noMarginTop');
         }
 }
 
 btnMessageProjetEtudiant.addEventListener('click', () => {
+        navBar.classList.add('noMarginTop');
+        navLinks.classList.add('noMarginTop');
         messageProjetEtudiant.classList.add('displayNone');
         localStorage.setItem('msgFermer', 'true');
         console.log('test');
