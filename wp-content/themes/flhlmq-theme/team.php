f06dd6d71b71c497bb056a39b27bcd7e98301130
Jxshvfx <spiteyhd@gmail.com>
2024-11-25 17:06:51 -0500

    debut team
    debut du team php, ajout des images

--- wp-content/themes/flhlmq-theme/team.php
+++ wp-content/themes/flhlmq-theme/team.php
@@ -1,7 +1,7 @@
 <?php 
 /**
- * 	Template Name: À propos
- * 	Identique à page, mais avec une barre latérale
+ * 	Template Name: teamTheme
+ * 	Template Post Type: team-member
  */
 
 get_header(); // Affiche header.php
@@ -10,16 +10,94 @@ if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ?
 	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
 	while ( have_posts() ) : the_post(); 
 ?>
+<?php 
+$member1 = get_field('team_member_1');
+$membername1 = $member1['member_name_1'];
+$memberimage1 = $member1['member_image_1'];
+?>
+<main>
+        <div class="hero equipe">
+            <h1 class="hero__titre titre">Notre Équipe</h1>
+            <div class="hero__bgHero bgHero">
+                <img class="hero__img img" src="./medias/img/accueil_carte_01.jpg" alt="hero image">
+            </div>
+            <p class="hero__page page"><a href="./index.html">Accueil</a> > Notre Équipe</p>
+        </div>
 
-	<article>
-		<?php if (!is_front_page()) : // Si nous ne sommes PAS sur la page d'accueil ?>
-			<h2>
-				<?php the_title(); // Titre de la page ?>
-			</h2>
-		<?php endif; ?>
-		
-		<?php the_content(); // Contenu principal de la page ?>
-	</article>
+        <div class="equipeConteneur">
+            <div class="membreEquipe">
+                <img loading="lazy" class="membreEquipe__img" id="membre1Btn"
+                    src="<?php echo esc_url( $memberimage1['image']['url'] ); ?>" alt="image robert_pilon">
+                <h1 class="membreEquipe__nom"><?php echo $membername1 ?></h1>
+                <p class="membreEquipe__poste"><?php the_field('member_role_1')?></p>
+            </div>
+
+            <div class="membreEquipe">
+                <img loading="lazy" class="membreEquipe__img" id="membre2Btn"
+                    src="<?php the_field('member_image_2')?>" alt="image elisabeth_pham">
+                <h1 class="membreEquipe__nom"><?php the_field('member_name_2')?></h1>
+                <p class="membreEquipe__poste"><?php the_field('member_role_2')?></p>
+            </div>
+
+            <div class="membreEquipe">
+                <img loading="lazy" class="membreEquipe__img" id="membre3Btn"
+                    src="<?php the_field('member_image_3')?>" alt="image patricia_viannay">
+                <h1 class="membreEquipe__nom"><?php the_field('member_name_3')?></h1>
+                <p class="membreEquipe__poste"><?php the_field('member_role_3')?></p>
+            </div>
+
+            <div class="membreEquipe">
+                <img loading="lazy" class="membreEquipe__img" id="membre4Btn"
+                    src="<?php the_field('member_image_4')?>" alt="image anik_leroux">
+                <h1 class="membreEquipe__nom"><?php the_field('member_name_4')?></h1>
+                <p class="membreEquipe__poste"><?php the_field('member_role_4')?></p>
+            </div>
+        </div>
+
+        <div class="modalMembreEquipe" id="membre1">
+            <button class="modalMembreEquipe__btnFermer" id="membre1BtnClose">X</button>
+            <img loading="lazy" class="modalMembreEquipe__img" id="membre1Btn"
+                src="<?php the_field('member_image_1')?>" alt="image robert_pilon">
+            <h1 class="modalMembreEquipe__nom"><?php the_field('member_name_1')?></h1>
+            <p class="modalMembreEquipe__poste"><?php the_field('member_role_1')?></p>
+            <p>
+			<?php the_field('member_desc_1')?>
+            </p>
+        </div>
+
+        <div class="modalMembreEquipe" id="membre2">
+            <button class="modalMembreEquipe__btnFermer" id="membre2BtnClose">X</button>
+            <img loading="lazy" class="modalMembreEquipe__img" id="membre1Btn"
+                src="<?php the_field('member_image_2')?>" alt="image elisabeth_pham">
+            <h1 class="modalMembreEquipe__nom"><?php the_field('member_name_2')?></h1>
+            <p class="modalMembreEquipe__poste"><?php the_field('member_role_2')?></p>
+            <p>
+			<?php the_field('member_desc_2')?>
+            </p>
+        </div>
+
+        <div class="modalMembreEquipe" id="membre3">
+            <button class="modalMembreEquipe__btnFermer" id="membre3BtnClose">X</button>
+            <img loading="lazy" class="modalMembreEquipe__img" id="membre1Btn"
+                src="<?php the_field('member_image_3')?>" alt="image patricia_viannay">
+            <h1 class="modalMembreEquipe__nom"><?php the_field('member_name_3')?></h1>
+            <p class="modalMembreEquipe__poste"><?php the_field('member_role_3')?></p>
+            <p>
+			<?php the_field('member_desc_3')?>
+            </p>
+        </div>
+
+        <div class="modalMembreEquipe" id="membre4">
+            <button class="modalMembreEquipe__btnFermer" id="membre4BtnClose">X</button>
+            <img loading="lazy" class="modalMembreEquipe__img" id="membre1Btn"
+                src="<?php the_field('member_image_4')?>" alt="image anik_leroux">
+            <h1 class="modalMembreEquipe__nom"><?php the_field('member_name_4')?></h1>
+            <p class="modalMembreEquipe__poste"><?php the_field('member_role_4')?></p>
+            <p>
+			<?php the_field('member_desc_4')?>
+            </p>
+        </div>
+    </main>
 <?php endwhile; // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
