5acc164ca83c374013f47aa66c2c24d512539fb8
KenzaElHarrif <112128295+KenzaElHarrif@users.noreply.github.com>
2024-12-08 17:30:04 -0500

    ajout temoins

---
+++ wp-content/themes/flhlmq-theme/partials/temoignage.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template Name: Temoignage
+ * Template Post Type: temoignage
+ */
+if ( have_posts() ) : // Est-ce que nous avons des pages à afficher ? 
+	// Si oui, bouclons au travers les pages (logiquement, il n'y en aura qu'une)
+	$temoin = new WP_Query('post_type=temoignage');
+    
+    while ($temoin->have_posts()) : $temoin->the_post();  
+    ?>
+    <div class="marge__tem">
+      <div class="temoignage temoignage--un">
+        <div class="temoignage__titre">
+          <?php the_title(); ?>
+          <p><?php the_field("job"); ?></p>
+        </div>
+        <div class="tem">
+          <div class="tem__img" style="background-image: url('<?php the_post_thumbnail_url(); ?>');"></div>
+          <div class="tem__info">
+          <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+      <?php 
+    endwhile;
+    ?>
+
+  
