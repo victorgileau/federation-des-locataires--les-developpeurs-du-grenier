3aed607989cfb663eab8074feb45297bfe7ad249
victorgileau <vgileau@hotmail.com>
2024-12-11 08:40:14 -0500

    Update news-hub.php

--- wp-content/themes/flhlmq-theme/news-hub.php
+++ wp-content/themes/flhlmq-theme/news-hub.php
@@ -10,51 +10,6 @@ get_template_part( 'partials/hero');
 
 ?>
 
-	<article>
-	<section class="actus">
-		<div class="actus__alignement">
-		<?php
-		if ( have_posts() ) : 
-			// Si oui, bouclons au travers pour tous les afficher
-			
-		$arguments = array( // 👈 Tableau d'arguments
-			'orderby' => array(
-			'date' =>'DESC',
-			),
-			'post_type' => 'news_article',
-		);
-		$i = 1;
-		$article = new WP_Query($arguments); // 👈 Utilisation
-		while ($article->have_posts()) : $article->the_post();
-		?>
-
-		<a href="<?php the_permalink(); ?>" class="actualiteHub <?php echo "news", $i ?>">
-				<p class="actualiteHub__date">
-				<?php echo esc_html ( get_field( 'newsarticledate' ) ); ?>
-				</p>
-				<div class="actualiteHub__desc">
-				<p>
-					<span class="surligne__droits"><?php the_title(); ?></span>
-				</p>
-				</div>
-
-				<div class="actualiteHub__img" style="background-image: url('<?php the_post_thumbnail_url();?>');"></div>
-				<?php $value = get_field('newsarticlecategory');?>
-				<button class="actualiteHub__btn btnN">
-				<?php echo $value[0]?>
-				</button>
-		</a>
-
-		<?php
-		$i++;
-		endwhile; 
-		wp_reset_postdata(); 
-		?>
-		</div>
-	</section>
-
-	</article>
-
 	<script>		
 		document.addEventListener("DOMContentLoaded", (event) => {
 			fetch("<?php bloginfo('url'); ?>/index.php/wp-json/wp/v2/news_article")
@@ -113,6 +68,53 @@ get_template_part( 'partials/hero');
 
 		</div>
 	</section>
+
+	<article>
+	<section class="actus">
+		<div class="actus__alignement">
+		<?php
+		if ( have_posts() ) : 
+			// Si oui, bouclons au travers pour tous les afficher
+			
+		$arguments = array( // 👈 Tableau d'arguments
+			'orderby' => array(
+			'date' =>'DESC',
+			),
+			'post_type' => 'news_article',
+		);
+		$i = 1;
+		$article = new WP_Query($arguments); // 👈 Utilisation
+		while ($article->have_posts()) : $article->the_post();
+		?>
+
+		<a href="<?php the_permalink(); ?>" class="actualiteHub <?php echo "news", $i ?>">
+				<p class="actualiteHub__date">
+				<?php echo esc_html ( get_field( 'newsarticledate' ) ); ?>
+				</p>
+				<div class="actualiteHub__desc">
+				<p>
+					<span class="surligne__droits"><?php the_title(); ?></span>
+				</p>
+				</div>
+
+				<div class="actualiteHub__img" style="background-image: url('<?php the_post_thumbnail_url();?>');"></div>
+				<?php $value = get_field('newsarticlecategory');?>
+				<button class="actualiteHub__btn btnN">
+				<?php echo $value[0]?>
+				</button>
+		</a>
+
+		<?php
+		$i++;
+		endwhile; 
+		wp_reset_postdata(); 
+		?>
+		</div>
+	</section>
+
+	</article>
+
+	
 <?php // Fermeture de la boucle
 
 else : // Si aucune page n'a été trouvée
